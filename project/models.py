20446655a0e87e47ae122e54eb3523ef50ac9b19
vladyslav.hnatchenko <hnatchenko.vladyslav@gmail.com>
2019-01-21 14:39:05 +0200

    1/21/19

--- project/models.py
+++ project/models.py
@@ -1,28 +1,22 @@
 from django.db import models
 
 
-class Person2(models.Model):
-    SHIRT_SIZES = (
-        ('S', 'Small'),
-        ('M', 'Medium'),
-        ('L', 'Large'),
-    )
-    name = models.CharField(max_length=60)
-    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth_date = models.DateField()
 
+    def baby_boomer_status(self):
+        """Returns the person's baby-boomer status."""
+        import datetime
+        if self.birth_date < datetime.date(1945, 8, 1):
+            return "Pre-boomer"
+        elif self.birth_date < datetime.date(1965, 1, 1):
+            return "Baby boomer"
+        else:
+            return "Post_boomer"
 
-# class Musician(models.Model):
-#     first_name = models.CharField(max_length=50)
-#     last_name = models.CharField(max_length=50)
-#     instrument = models.CharField(max_length=100)
-#
-#
-# class Album(models.Model):
-#     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
-#     name = models.CharField(max_length=100)
-#     release_date = models.DateField()
-#     num_stars = models.IntegerField()
-
-# class Person(models.Model):
-#     first_name = models.CharField(max_length=30)
-#     last_name = models.CharField(max_length=30)
+    @property
+    def full_name(self):
+        """Returns the person's full name."""
+        return '%s %s' % (self.first_name, self.last_name)
