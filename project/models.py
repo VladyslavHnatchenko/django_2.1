8eb6b36f205dc3ae3d3cfa306b22c1ac0005be74
vladyslav.hnatchenko <hnatchenko.vladyslav@gmail.com>
2019-01-22 13:55:25 +0200

    1/22/19

--- project/models.py
+++ project/models.py
@@ -1,17 +1,32 @@
 from django.db import models
 
 
-class Piece(models.Model):
-    pass
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 
-class Article(Piece):
-    article_piece = models.OneToOneField(Piece, on_delete=models.CASCADE, parent_link=True)
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
 
 
-class Book(Piece):
-    book_piece = models.OneToOneField(Piece, on_delete=models.CASCADE, parent_link=True)
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField()
+    authors = models.ManyToManyField(Author)
+    n_comments = models.IntegerField()
+    n_pingbacks = models.IntegerField()
+    rating = models.IntegerField()
 
-
-class BookReview(Book, Article):
-    pass
+    def __str__(self):
+        return self.headline
